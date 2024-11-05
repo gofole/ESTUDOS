f54484b5c9ae72df046a16a79865e97c02d16e27
gofole <gofole157@gmail.com>
2024-11-04 22:43:47 -0300

    test

--- Python com IA/Listas e Tuplas.py
+++ Python com IA/Listas e Tuplas.py
@@ -1,22 +1,115 @@
-# for variavel in range('inicio,fim,passo'):
-#     print(variavel)
+# minha_lista = [10,20,30,40,50]
+
+# print(minha_lista[0])
+
+# print(minha_lista[2])
+
+# minha_lista =[10,20,30]
+# minha_lista[1]=200
+# print(minha_lista)
+
+
+# minha_lista = [1,2,3,]
+# minha_lista.append(4)
+
+# print(minha_lista)
+# minha_lista.insert(1,'a')
+# print(minha_lista)
+
+
+
+# minha_lista =[1,2,3,4,5]
+# minha_lista.remove(3)
+# print(minha_lista)
+# minha_lista.pop(1)
+# print(minha_lista)
+# minha_lista.clear()
+# print(minha_lista)
+
+
+
+
+# meus_brinquedos = ['carro','boneca','bola']
+# # tamanho = len(meus_brinquedos)
+# print(len(meus_brinquedos))
+
+
+# frutas = ['maças','banana','cereja']
+# for fruta in frutas:
+#     print(fruta.upper())
+
+# nomes = input('Digite Seu nome')
+# for i in nomes:
+#     tamanho = len(nomes)
+
+# print(f'Ola {nomes} Seu nome tem {tamanho} comprimento de Letras')
+
+
+# grupos = [
+#     ['alice','bob','charlie'],
+#     ['david','eva','frank'],
+#     ['grace','hannah','ivan']
+#     ]
+# print(grupos[1][0])
+# print(grupos[2][1])
+
+
+# grupos = [
+#     ['alice','bob','charlie'],
+#     ['david','eva','frank'],
+#     ['grace','hannah','ivan']
+#     ]
+
+# for grupo in grupos:
+#     for aluno in grupo:
+#         print(aluno.upper())
+
+
+
+
+# grupos = [
+#     ['alice','bob','charlie'],
+#     ['david','eva','frank'],
+#     ['grace','hannah','ivan']
+#     ]
+# grupos[1][0] = 'daniel'
+# grupos[2][2] = 'Isaac'
+# print(grupos)
+
+
+
+# minha_tupla =(10,20,30,40,50)
+# print(minha_tupla[0])
+
+
+# minha_tupla =(1,2,3)
+# minha_tupla[1] = 200
+
+
+# coordenadas = (10,20)
+# configuraçao = ('modo_escuro',True)
+
+
+
+# lista =[1,2,3,4,5,]
+# lista.append(6)
+# lista.pop(3)
+# print(lista)
+# cidades = ('belo horizonte','sabara','nova Lima')
+# print(cidades)
+
 
 # for i in range(0,6):
-#     print(i)
+#         numeros = [int(input('Digite Um Numero'))]
+#         print(numeros)
 
 
-# numero_procurado =7
-# for i in range(1,11):
-#     if i == numero_procurado:
-#         print(f' Numero procurado é {numero_procurado}  encontrado com Sucesso')
+# lista = [10,20,30,40,50]
+# for i in lista:
+#     if i == 30:
+#         print('O 30 Esta na Lista')
 #         break
-#     print(i)
-
-
-# for i in range(1,11):
-#     if i %2 != 0:
-#         continue
-#     print(i)
-
+# else:
+#     print('O 30 NAO ESTA NA LISTA!')
 
 
