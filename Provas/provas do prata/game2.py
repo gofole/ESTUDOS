0c746111d5508f2f59db0695f620861a0dac0e65
gofole <gofole157@gmail.com>
2024-11-13 03:28:32 -0300

    game2

---
+++ Provas/provas do prata/game2.py
@@ -0,0 +1,23 @@
+import random
+opcoes =["pedra","papel","tesoura"]
+escolha = random.choice(opcoes)
+while True:
+        jogador = input("Escolha pedra, papel ou tesoura (ou 'sair' para encerrar o jogo): ")
+        
+        if jogador == "sair":
+            print("O jogo foi encerrado.")
+            break
+        
+        if jogador not in opcoes:
+            print("Escolha inválida. Tente novamente.")
+            continue
+        
+        computador = random.choice(opcoes)
+        print(f"O computador escolheu: {computador}")               #AI E FODA EM PRATA!!!NUNCA MAIS! 14HORAS PRA FAZER ISSO
+
+        if jogador == computador:
+            print("Empate!")
+        elif (jogador == "pedra" and computador == "tesoura") or (jogador == "papel" and computador == "pedra") or (jogador == "tesoura" and computador == "papel"):
+            print("Você ganhou!")
+        else:
+            print("Você perdeu!")
